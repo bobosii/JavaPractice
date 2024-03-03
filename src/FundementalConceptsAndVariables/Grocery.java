404377201e81a69d971d09d4a10435f1a8d39ab0
bobosii <day.emir@hotmail.com>
2024-03-03 19:18:06 +0300

    Basic GroceryCalculator

---
+++ src/FundementalConceptsAndVariables/Grocery.java
@@ -0,0 +1,23 @@
+package FundementalConceptsAndVariables;
+import java.util.Scanner;
+public class Grocery {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double armut = 2.14, elma = 3.67, domates = 1.11, muz = 0.95, patlican = 5,total;
+
+        System.out.print("Kaç kg armut aldınız ? ");
+        double armutKg = input.nextDouble();
+        System.out.print("Kaç kg elma aldınız ? ");
+        double elmaKg = input.nextDouble();
+        System.out.print("Kaç kg domates aldınız ? ");
+        double domatesKg = input.nextDouble();
+        System.out.print("Kaç kg muz aldınız ? ");
+        double muzKg = input.nextDouble();
+        System.out.print("Kaç kg patlican aldınız ? ");
+        double patlicanKg = input.nextDouble();
+
+        total = armut * armutKg + elma * elmaKg + domates * domatesKg + muz * muzKg + patlican * patlicanKg;
+        System.out.println("Toplam fiyat = " + total);
+
+    }
+}
