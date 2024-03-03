a41e6ae10befa0dabbd770d82c74e890d54da4e6
bobosii <day.emir@hotmail.com>
2024-03-03 18:53:29 +0300

    TaxiMeter updated

--- src/FundementalConceptsAndVariables/TaxiMeter.java
+++ src/FundementalConceptsAndVariables/TaxiMeter.java
@@ -2,24 +2,16 @@ package FundementalConceptsAndVariables;
 import java.util.Scanner;
 public class TaxiMeter {
     public static void main(String[] args) {
-
-        
+        // Taksimetre başlangıcı 10 tl, km başına ise 2.20 ücret alınıyor minimum tutar 20 tl.
         double pricePerKm,totalKm,totalPrice,firstPrice;
         firstPrice = 10;
         Scanner inp = new Scanner(System.in);
 
         pricePerKm = 2.20;
-
         System.out.print("Kaç km yol gideceksiniz ? ");
         totalKm = inp.nextDouble();
         totalPrice = totalKm * pricePerKm + firstPrice;
-        if (totalPrice<20){
-            System.out.println("Toplam ödenecek ücretiniz : 20 TL");
-        }else {
-            System.out.println("Toplam ödenecek Ücretiniz : " + totalPrice);
-        }
-
-
-
+        totalPrice = (totalPrice<20) ? 20 : totalPrice;
+        System.out.println("Toplam ödenecek tutar : " + totalPrice);
     }
 }
