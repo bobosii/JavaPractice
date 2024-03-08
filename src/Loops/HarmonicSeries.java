d5522afe24dc2f24be371cc89b02d99e6ebe971b
bobosii <day.emir@hotmail.com>
2024-03-08 21:54:30 +0300

    HarmonicSeries

---
+++ src/Loops/HarmonicSeries.java
@@ -0,0 +1,17 @@
+package Loops;
+import java.util.Scanner;
+public class HarmonicSeries {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int a;
+        double result = 0;
+        System.out.print("Enter a number : ");
+        a = inp.nextInt();
+
+        for (double i = 1; i<=a; i++){
+            result += (1/i);
+        }
+        System.out.println(result);
+
+    }
+}
